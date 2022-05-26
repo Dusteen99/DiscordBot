b236e018966262d9438bf419d26ef2f28470091e
Dusteen99 <dusteendev@gmail.com>
2022-05-25 21:32:06 -0400

    some files removed, typos fixed, working basic version

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var auth = require('./auth.json');
 
 //configure logger settings
 
-logger.remote(logger.transports.Console);
+logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console,{
     colorize: true
 
@@ -31,7 +31,7 @@ bot.on('message', function(user, userID, channelID, message, evt){
     //Out bot needs to know if it will execute a command
     //It will listen for messages that start with %
 
-    if(message.substrong(0,1) == '%'){
+    if(message.substring(0,1) == '%'){
         var args = message.substring(1).split(' ');
         var cmd = args[0];
         args = args.splice(1);
