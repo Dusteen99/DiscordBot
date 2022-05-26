784739e3dbff6ec8d42a17552f73539902cc154a
Dusteen99 <dusteendev@gmail.com>
2022-05-25 21:25:27 -0400

    Initial commit with basic functionality

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DiscordBot
 A discord bot that had pokemon-related features
+
+This will allow the collecting of cards and random generation of other pokemon to catch.
+
+
---
+++ bot.js
@@ -0,0 +1,51 @@
+var Discord = require('discord.io');
+
+var logger = require('winston');
+
+var auth = require('./auth.json');
+
+//configure logger settings
+
+logger.remote(logger.transports.Console);
+logger.add(new logger.transports.Console,{
+    colorize: true
+
+});
+
+logger.level = 'debug';
+
+//Initialize discord bot
+
+var bot = new Discord.Client({
+    token: auth.token,
+    autorun:true
+});
+
+bot.on('ready', function (evt){
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+
+bot.on('message', function(user, userID, channelID, message, evt){
+    //Out bot needs to know if it will execute a command
+    //It will listen for messages that start with %
+
+    if(message.substrong(0,1) == '%'){
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        args = args.splice(1);
+
+        switch(cmd){
+            //%ping
+
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+                break;
+            //more commands here
+        }
+    }
+});
